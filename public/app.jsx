578fdda585b77b3d44b83dd1ebfa39fe7cc30e58
塏翔 潘 <settour.frontend@gmail.com>
2017-05-09 18:09:32 +0800

    this is a useless project but just pratice react you know

--- public/app.jsx
+++ public/app.jsx
@@ -21,22 +21,38 @@ var GreeterForm = React.createClass({
     onFormSubmit: function(e){
       e.preventDefault();//預防預設的動作，也就是browser重整
      
-     var updates = {};
-     
+      var updates = {};//更新user輸入的值所使用的object
+
       var name = this.refs.name.value;//從下面<input type="text" ref="name" />傳遞props
+      var message = this.refs.message.value;
       if (name.length > 0){//如果我們輸入的字串大於0
         this.refs.name.value = '';//刷新讓輸入框變回0
-        updates.name = name;
-       
+        updates.name = name;  
       }
+      
+      if(message.length > 0){//如果我們輸入的字串大於0
+        this.refs.message.value = '';//刷新讓輸入框變回0
+        updates.message = message;//更新user輸入的值，藉由定義updates object的property
+      }
+
        //接收到此元件的props並觸動事件，且使用這邊的locoal變數name也就是將經過input傳遞過來的字串
-        this.props.onNewName(name);
+        this.props.onNewData(updates);//將改變的值傳遞去Greeter componert
     },
     render: function(){
        return (
         <form onSubmit={this.onFormSubmit}>
-            <input type="text" ref="name" />
-            <button>輸入姓名</button>
+            <div>
+              <input type="text" ref="name" placeholder="輸入姓名"/>
+            </div>
+
+            <div>
+              <textarea ref="message" placeholder="輸入留言"></textarea>
+            </div>
+            
+            <div>
+               <button>提交資料</button>
+            </div>
+            
           </form>
        );
     }
@@ -49,36 +65,35 @@ var Greeter = React.createClass({
 //預設props值
  getDefaultProps: function(){
       return {
-          name: 'React',
-          message: 'hello who are you ?'
+          name: '請輸入姓名',
+          message: '請輸入留言'
       };
     },
 
 //設定元件內的state狀態 
   getInitialState: function(){
     return {
-        name: this.props.name//將name的state 設為等於屬性名的value，透過props傳過來也就是字串Paul
-    };
+        name: this.props.name,//將name的state 設為等於屬性名的value，透過props傳過來也就是字串Paul
+        message: this.props.message  
+  };
   },
 
  //點擊事件區塊
-      handleNewName: function(name){
-         this.setState({
-           name: name 
+      handleNewData: function(updates){
+         this.setState(updates);
            //讓這個property等於此function的參數name也就是user輸入的值
            //，是經過另外一個component更新的
            //而這個method將會更新state，讓render出去的東西跟著改變
-         });
       },
 
 //render出去的
   render: function(){
     var name = this.state.name;//fetch上面的state值
-    var message = this.props.message;//一定要記得使用this.props.屬性名
+    var message = this.state.message;//一定要記得使用this.props.屬性名
      return (
         <div>
           <GreeterMessage name={name} message = {message}/>  
-          <GreeterForm onNewName={this.handleNewName} />
+          <GreeterForm onNewData={this.handleNewData} />
         </div>
       );
     }
